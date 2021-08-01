cbf22d1b0747f85466e6b171471ae59b469706da
shindonghwi <ehdgnl8940@naver.com>
2021-08-01 13:19:13 +0900

    chore: version upgrade 0.0.6

--- app/wolf_imageutil_lib/src/main/java/dh/wolf/wolf_imageutil_lib/WolfImageUtil.kt
+++ app/wolf_imageutil_lib/src/main/java/dh/wolf/wolf_imageutil_lib/WolfImageUtil.kt
@@ -1,8 +1,16 @@
 package dh.wolf.wolf_imageutil_lib
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.widget.Toast
 import java.io.ByteArrayOutputStream
+
+class ToastTest{
+    companion object{
+        fun showToast(context: Context, msg: String) = Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    }
+}
 
 object WolfImageUtil : IWolfImage {
 
