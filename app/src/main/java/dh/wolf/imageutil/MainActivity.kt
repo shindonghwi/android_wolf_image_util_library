e1420910961eef51c76c2692734eca042ae005f0
shindonghwi <ehdgnl8940@naver.com>
2021-08-01 11:02:45 +0900

    Initial commit

---
+++ app/src/main/java/dh/wolf/imageutil/MainActivity.kt
@@ -0,0 +1,11 @@
+package dh.wolf.imageutil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
